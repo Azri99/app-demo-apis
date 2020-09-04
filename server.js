04bd8b5d0439c2f5451ef0a62d757aab669c3862
Ahmad Azri <azrirazak01@gmail.com>
2020-09-04 14:41:06 +0800

    configure helmet

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const app = require('express')();
 const cors = require('cors');
-
+const helmet  = require('helmet');
 const webServer = require('./services/web-server');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(cors({origin:true}));
 
 app.use('/api', webServer);
